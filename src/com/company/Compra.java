21bed9fdd711ec3cfe88efe1b57f617679719163
nahuelz123 <84518078+nahuelz123@users.noreply.github.com>
2021-06-07 21:32:52 -0300

    v1.8 hice generica la clase carrito, falta agregar algunos metodos

--- src/com/company/Compra.java
+++ src/com/company/Compra.java
@@ -1,4 +1,52 @@
 package com.company;
 
 public class Compra {
+	private String producto;
+	private int cantidad;
+	private double precioTotal;
+	/**
+	 * @return the producto
+	 */
+	public String getProducto() {
+		return producto;
+	}
+	/**
+	 * @param producto the producto to set
+	 */
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	/**
+	 * @return the cantidad
+	 */
+	public int getCantidad() {
+		return cantidad;
+	}
+	/**
+	 * @param cantidad the cantidad to set
+	 */
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	/**
+	 * @return the precioTotal
+	 */
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+	/**
+	 * @param precioTotal the precioTotal to set
+	 */
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	@Override
+	public String toString() {
+		return "Compra [getProducto()=" + getProducto() + ", getCantidad()=" + getCantidad() + ", getPrecioTotal()="
+				+ getPrecioTotal() + "]";
+	}
+	
+	
+	
+	
 }
